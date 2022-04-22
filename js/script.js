e0bb1618c4036d933e628cb9b3e7f2c0f79f78e3
Mariya Tumelya <mtumelia@gmail.com>
2022-04-22 23:41:28 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const contents = document.querySelectorAll('.program-line__content');
+
+//console.log(contents);
+
+contents.forEach((elem) => {
+    const title = elem.querySelector('.program-line__title');
+    const descr = elem.querySelector('.program-line__descr');
+
+    title.addEventListener('click', () => {
+        descr.classList.toggle('active');
+        //console.log(title);
+    })
+
+    //console.log(descr);
+})
