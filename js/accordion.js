ed5bd2c5eb17277b3872121d453ccfe779036714
Mariya Tumelya <mtumelia@gmail.com>
2022-04-24 18:46:14 +0300

    Task 3 - timer
    Adding a timer. Changing the appearance of the discription when you click on the weeks in accordion

---
+++ js/accordion.js
@@ -0,0 +1,21 @@
+const contents = document.querySelectorAll('.program-line__content');
+const contentsDescr = document.querySelectorAll('.program-line__descr');
+
+contents.forEach((elem) => { 
+    const title = elem.querySelector('.program-line__title');            
+    const descr = elem.querySelector('.program-line__descr');     
+    
+    descr.style.transition = 'all .3s';
+    descr.style.overflow = 'hidden';
+
+    title.addEventListener('click', () => {                             
+        contentsDescr.forEach((tab) => {                                      
+            if (tab === descr) {                                           
+                //tab.classList.toggle('active'); 
+                tab.style.height = tab.scrollHeight + 'px';                       
+            } else { //tab.classList.remove('active');  
+            tab.style.height = 0;
+            }                   
+        })
+    })
+})
