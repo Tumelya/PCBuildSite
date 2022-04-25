38cbcbf1e33b322de7a2a81663300584e543929a
Mariya Tumelya <mtumelia@gmail.com>
2022-04-25 12:49:15 +0300

    cross
    adding a cross to close the modal window

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,30 @@
 const modalBtn = document.querySelector('.modal__button');
 const modal = document.querySelector('.modal ');
+const modalInner = modal.querySelector('.modal__inner');
+
+modalInner.style.position = 'relative';
+
+const addCloseBtn = () => {
+    const closeBtn = document.createElement('div');
+    closeBtn.classList.add('close-button');
+    closeBtn.innerHTML = '&times';
+    closeBtn.style.position = 'absolute';
+    closeBtn.style.right = '10px';
+    closeBtn.style.top = '10px';
+    closeBtn.style.width = '20px';
+    closeBtn.style.height = '20px';
+    closeBtn.style.fontSize = '25px';
+    closeBtn.style.cursor = 'pointer';
+    closeBtn.style.display = 'flex';
+    closeBtn.style.justifyContent = 'center';
+    closeBtn.style.alignItems = 'center';
+
+    modalInner.append(closeBtn);
+
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = '';
+    })
+}
 
 modalBtn.addEventListener('click', () => {
     modal.style.display = 'flex';
@@ -12,3 +37,5 @@ modal.addEventListener('click', (event) => {
         modal.style.display = '';
     }
 })
+
+addCloseBtn();
